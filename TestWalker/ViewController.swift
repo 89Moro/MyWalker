0e5406809c709a69233021ed015e0481089f97ea
Ospite <ospite@imac-di-diskos14.local>
2017-05-26 17:35:59 +0200

    bum bum

--- TestWalker/ViewController.swift
+++ TestWalker/ViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 import MapKit
 import CoreLocation
 import SQLite
-
+//bum bum
 class ViewController: UIViewController,MKMapViewDelegate
 {
     @IBOutlet weak var btnStart: UIButton!
     @IBOutlet weak var btnStop: UIButton!
     @IBOutlet weak var mvMap: MKMapView!
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         mvMap.delegate = self
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,6 +29,16 @@ class ViewController: UIViewController,MKMapViewDelegate
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager:CLLocationManager, didUpdateLocations locations : [CLLocation])
+    {
+        let currentLat = (manager.location?.coordinate.latitude)
+        let currentLon = (manager.location?.coordinate.longitude)
+        
+        let newCoordinate = CLLocation(latitude : currentLat!,longitude: currentLon! ).coordinate
+        
+        
     }
 
 
